255f08feb4c7abfa27a635fb569e585c03a594eb
LarissaBiff <biff.larissa@gmail.com>
2026-03-17 11:56:13 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+const tarefa = ref([
+    { id: 1, desc: 'Prova Geografia', status: 'pendente' },
+    { id: 2, desc: 'Prova História', status: 'concluida' },
+    { id: 3, desc: 'Trabalho DevWeb', status: 'pendente' },
+]);
+</script>
+
+<template>
+<div class="container">
+    <ul>
+        <li v-for="item in tarefas" :key="item.id">
+            {{ item.desc }}
+        </li>
+    </ul>
+</div>
+</template>
+
+<style scoped>
+</style>
